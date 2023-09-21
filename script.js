923313b280ea67eb6a89816ef1589359a3db7bf8
SantosN21 <santosjnovoa21@gmail.com>
2023-09-21 03:58:28 -0700

    top commit

--- script.js
+++ script.js
@@ -6,11 +6,13 @@ const myLibrary = [
   new Book("Fahrenheit 451", "Ray Bradbury", 272, false),
 ];
 
-function Book(title, author, pages, read) {
+class Book {
+  constructor(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read ? "Read" : "Not Read";
+  }
 }
 
 const toggleReadStatus = (book, buttonElement) => {
